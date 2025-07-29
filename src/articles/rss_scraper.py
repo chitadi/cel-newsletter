684df3d126ce26dfc28e002e770d2c9653a87f33
chitadi <chittemadithya@gmail.com>
2025-07-29 10:03:06 +0530

    o. changed it two days because the newsletter file became too big

--- src/articles/rss_scraper.py
+++ src/articles/rss_scraper.py
@@ -8,7 +8,7 @@ UTC = pytz.utc
 
 UA = {"User-Agent": "Mozilla/5.0"}
 
-def fetch_rss(source: dict, db: Session, horizon_hours=72):
+def fetch_rss(source: dict, db: Session, horizon_hours=48):
     cutoff = datetime.datetime.now(tz=UTC) - datetime.timedelta(hours=horizon_hours)
     feed = feedparser.parse(source["feed_url"])
     
